a9c029b4f83aeca1934d126ee289edce64040622
Abbas Valliani <valliania@yahoo.com>
2022-08-04 16:05:13 -0400

    Changed mods

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 # Startup Xvfb so an actually headless machine can pretend to have a display
 Xvfb -ac :99 -screen 0 1280x1024x16 > /dev/null 2>&1 &
 
+
 # Export some variables
 export DISPLAY=:99.0
 #export PUPPETEER_EXEC_PATH="chromium"
